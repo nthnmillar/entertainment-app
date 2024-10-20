ea56c7335889397ab1a9204c14b6a3b6dfeeae03
Nathan Millar <nthnmillar@gmail.com>
2024-10-20 16:35:06 +1100

    greyish blue variable

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -2,16 +2,18 @@ import React from "react";
 
 const Login: React.FC = () => {
   return (
-    <>
-      <p> Login</p>
-      <br />
-      <p> Email address</p>
-      <p> Password</p>
-      <br />
-      <p> Login to your account</p>
-      <br />
-      <p> Don't have an account? Sign Up</p>
-    </>
+    <main>
+      <div className="lg:w-[300px] lg:h-[500px] lg:bg-greyishblue m-auto lg:mt-30px rounded-xl lg:mt-[10vh] p-[30px]">
+        <p> Login</p>
+        <br />
+        <p> Email address</p>
+        <p> Password</p>
+        <br />
+        <p> Login to your account</p>
+        <br />
+        <p> Don't have an account? Sign Up</p>
+      </div>
+    </main>
   );
 };
 
